64013681594fd29ba700342dce9e77e2793db37f
okiaahhbb <okiagoni613@gmail.com>
2025-06-08 07:47:29 +0600

    dashboard modifying and some file delete

--- AdminofOLM/admin_login.php
+++ AdminofOLM/admin_login.php
@@ -1,24 +1,32 @@
 <?php
+session_start();
 include('Header.php');
 include('connection.php');
 
-$message = "";
-
-if (isset($_POST['submit'])) {
+if(isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
-
-    $query = "SELECT * FROM student WHERE username='$username' AND password='$password'";
-    $result = mysqli_query($db, $query);
-
-    if (mysqli_num_rows($result) > 0) {
-        header("Location: student_dashboard.php");
-        exit();
-    } else {
-        $message = "Invalid username or password.";
+    
+    $stmt = $db->prepare("SELECT * FROM admin WHERE username=?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    if($result->num_rows == 1) {
+        $admin = $result->fetch_assoc();
+        if(password_verify($password, $admin['password'])) {
+            $_SESSION['admin_logged_in'] = true;
+            $_SESSION['admin_id'] = $admin['id'];
+            header("Location: admin_dashboard.php");
+            exit();
+        }
     }
+    
+    $error = "Invalid username or password";
 }
 ?>
+
+
 
 <style>
     body {
